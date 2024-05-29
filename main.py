f38ff367e090b58fa858236e55c371806fc1e6e9
danylo8 <ozai.avatar@gmail.com>
2024-05-29 09:25:48 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -24,18 +24,19 @@ g1=Gameicon1(100,50)
 g2=Gameicon2(105,230)
 g3=Gameicon3(380,50)
 g4=Gameicon4(380,230)
+s1 = Surviver1(0, 0)
 
 keys = pygame.key.get_pressed()
 mouse_click = pygame.mouse.get_pressed()
 visible=True
-b1=pygame.image.load("gameicon_volcano.jpg")
-b2= pygame.image.load("gameicon_tsunami.png")
+b2=pygame.image.load("gameicon_volcano.jpg")
+b1= pygame.image.load("gameicon_tsunami.png")
 b3= pygame.image.load("gameicon_meteor.png")
 b4= pygame.image.load("gameicon_tornado.png.")
 
 g1_bg = pygame.image.load("tsunami_g1_background.jpg")
-g2_bg= pygame.image.load("meteor game background.jpg")
-g4_bg= pygame.image.load("volcano game background.jpg")
+g3_bg= pygame.image.load("meteor game background.jpg")
+g2_bg= pygame.image.load("volcano game background.jpg")
 
 start_disaster_survival= my_font.render("Welcome to Natural Disaster Survival!", True, (200, 100, 0))
 to_start= my_font.render("To start, press the first mouse button", True, (200, 100, 0))
@@ -55,25 +56,23 @@ while run:
         if keys[pygame.K_s]:
             s1.move_direction("down")
 
-    for event in pygame.event.get():  # User did something
-        if event.type == pygame.MOUSEBUTTONUP:
-            start_game = True
-            start=True
-        if event.type == pygame.QUIT:  # If user clicked close
-            run = False
     (x,y)=pygame.mouse.get_pos()
-    if visible==True and g1.rect.collidepoint(x, y) and mouse_click[0]:
-        visible=False
-        s1 = Surviver1(0,0)
-        screen.blit(g1_bg, (0, 0))
-
-
     if visible==True and g2.rect.collidepoint(x, y) and mouse_click[0]:
         visible=False
     if visible==True and g3.rect.collidepoint(x, y) and mouse_click[0]:
         visible=False
     if visible==True and g4.rect.collidepoint(x, y) and mouse_click[0]:
         visible=False
+
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.MOUSEBUTTONUP:
+            start_game = True
+            start=True
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+
+
+
 
 
 
@@ -83,8 +82,25 @@ while run:
 
     keys = pygame.key.get_pressed()
     mouse_click = pygame.mouse.get_pressed()
+    if visible == True and g1.rect.collidepoint(x, y) and mouse_click[0] and start==True and start_game==True:
+        visible = False
+        screen.blit(g1_bg, (0, 0))
+        screen.blit(s1.image, s1.rect)
+    if visible == True and g2.rect.collidepoint(x, y) and mouse_click[0] and start==True and start_game==True:
+        visible = False
+
+        screen.blit(g2_bg, (0, 0))
+        screen.blit(s1.image, s1.rect)
+
+    if visible == True and g3.rect.collidepoint(x, y) and mouse_click[0] and start==True and start_game==True:
+        visible = False
+
+        screen.blit(g3_bg, (0, 0))
+        screen.blit(s1.image, s1.rect)
+
     if visible==True and mouse_click[0] and start==False:
         start=True
+        start_game=True
         screen.blit(g1.image, g1.rect)
         screen.blit(g2.image, g2.rect)
         screen.blit(g3.image, g3.rect)
