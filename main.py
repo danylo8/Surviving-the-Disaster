c5380a8358ef288bed9d636d4265959eb539c1ce
danylo8 <ozai.avatar@gmail.com>
2024-06-03 09:29:21 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -18,8 +18,12 @@ SCREEN_HEIGHT = 431
 SCREEN_WIDTH = 654
 size=(SCREEN_WIDTH, SCREEN_HEIGHT)
 screen=pygame.display.set_mode(size)
+
+scroll_x_axis=0
+scroll_speed=2
 start_game=False
 start=False
+
 g1=Gameicon1(100,50)
 g2=Gameicon2(105,230)
 g3=Gameicon3(380,50)
@@ -29,12 +33,10 @@ s1 = Surviver1(0, 0)
 keys = pygame.key.get_pressed()
 mouse_click = pygame.mouse.get_pressed()
 visible=True
-b2=pygame.image.load("gameicon_volcano.jpg")
-b1= pygame.image.load("gameicon_tsunami.png")
-b3= pygame.image.load("gameicon_meteor.png")
-b4= pygame.image.load("gameicon_tornado.png.")
 
-g1_bg = pygame.image.load("tsunami_g1_background.jpg")
+
+
+g1_bg = pygame.image.load("tsunami_g1_background.jpg").convert()
 
 g3_bg= pygame.image.load("meteor game background.jpg")
 g2_bg= pygame.image.load("volcano game background.jpg")
@@ -45,29 +47,24 @@ start_game=False
 run=True
 frame = 0
 while run:
-    scroll_x = 0
-    scroll_y = 0
-    background_x = 0
-    scroll_x -= 1
-    background_x -= 1
-
-
-
-    # Reset the background position when it goes off screen
-    if scroll_x <= -SCREEN_WIDTH:
-        scroll_x = SCREEN_WIDTH
-
-    if background_x <= -SCREEN_WIDTH:
-        background_x = SCREEN_WIDTH
     keys=pygame.key.get_pressed()
     if keys[pygame.K_d]:
         s1.move_direction("right")
+        scroll_x_axis=scroll_speed-1
     if keys[pygame.K_a]:
         s1.move_direction("left")
     if keys[pygame.K_w]:
         s1.move_direction("up")
     if keys[pygame.K_s]:
         s1.move_direction("down")
+
+    if scroll_x_axis <= -SCREEN_WIDTH:
+        scroll_x_axis = 0
+
+    if scroll_x_axis >= SCREEN_WIDTH:
+        scroll_x_axis=0
+
+
 
     (x,y)=pygame.mouse.get_pos()
     if visible==True and g2.rect.collidepoint(x, y) and mouse_click[0]:
@@ -80,26 +77,31 @@ while run:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.MOUSEBUTTONUP:
             start_game = True
-            start=True
+
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
 
 
 
 
+    screen.fill((0,0,0))
 
-
-    if start==False:
+    if start_game==False:
         screen.blit(start_disaster_survival,(0,0))
-        visible=True
+
+        screen.blit(g1.image, g1.rect)
+        screen.blit(g2.image, g2.rect)
+        screen.blit(g3.image, g3.rect)
+        screen.blit(g4.image, g4.rect)
+        visible=False
 
     keys = pygame.key.get_pressed()
     mouse_click = pygame.mouse.get_pressed()
-    if visible == True and g1.rect.collidepoint(x, y) and mouse_click[0] and start==True and start_game==True:
-        visible = False
+    if start_game==True:
 
-        screen.blit(g1_bg, (scroll_x, scroll_y))
-        screen.blit(g1_bg, (background_x, scroll_y))
+        screen.blit(g1_bg, (scroll_x_axis, 0))
+        screen.blit(g1_bg, (scroll_x_axis+SCREEN_WIDTH, 0))
+        screen.blit(s1.image,s1.rect)
 
     if visible == True and g2.rect.collidepoint(x, y) and mouse_click[0] and start==True and start_game==True:
         visible = False
@@ -113,15 +115,7 @@ while run:
         screen.blit(g3_bg, (0, 0))
         screen.blit(s1.image, s1.rect)
 
-    if visible==True and mouse_click[0] and start==False:
-        start=True
-        start_game=True
-        screen.blit(g1.image, g1.rect)
-        screen.blit(g2.image, g2.rect)
-        screen.blit(g3.image, g3.rect)
-        screen.blit(g4.image, g4.rect)
-    if visible==False:
-        screen.blit(s1.image, s1.rect)
+
     pygame.display.update()
     frame += 1
 
